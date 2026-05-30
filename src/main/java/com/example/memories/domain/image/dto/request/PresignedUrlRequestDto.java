27749677df6ab6fd9e523953bc7092828219c6d2
seongwon02 <seongwon2303@gmail.com>
2026-05-30 22:52:45 +0900

    feat: 이미지 업로드 갯수를 최대 4개로 제한

--- src/main/java/com/example/memories/domain/image/dto/request/PresignedUrlRequestDto.java
+++ src/main/java/com/example/memories/domain/image/dto/request/PresignedUrlRequestDto.java
@@ -4,14 +4,19 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record PresignedUrlRequestDto(
-        @NotEmpty List<@Valid FileRequest> files
+        @NotEmpty
+        @Size(max = 5, message = "한번에 최대 5개의 이미지만 업로드할 수 있습니다.")
+        List<@Valid FileRequest> files
 ) {
     public record FileRequest(
-            @NotBlank String fileName,
+            @NotBlank
+            String fileName,
+
             @NotBlank
             @Pattern(
                     regexp = "image/(jpeg|png|webp|gif|heic)",
