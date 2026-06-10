ff437a3e4295fb617929740661cf42cc3ac31e28
seongwon02 <seongwon2303@gmail.com>
2026-06-10 15:32:32 +0900

    refactor: oauth 시 토큰이 아닌 코드를 받을 수 있도록 변경

--- src/main/java/com/example/memories/domain/auth/dto/request/SocialLoginRequestDto.java
+++ src/main/java/com/example/memories/domain/auth/dto/request/SocialLoginRequestDto.java
@@ -3,5 +3,5 @@ package com.example.memories.domain.auth.dto.request;
 import jakarta.validation.constraints.NotBlank;
 
 public record SocialLoginRequestDto(
-        @NotBlank String token
+        @NotBlank String code
 ) {}
